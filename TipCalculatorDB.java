dbe7a5c2a72961bc7bfb665055030b25b84be168
Anu Slorah <anuslorah@hotmail.com>
2017-07-01 12:38:37 -0700

    Initial commit

---
+++ TipCalculatorDB.java
@@ -0,0 +1,150 @@
+package com.murach.tipcalculator;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class TipCalculatorDB {
+
+    //database object and database helper object
+    private SQLiteDatabase db;
+    private DBHelper dbHelper;
+    //database constants
+    public static final String DB_NAME = "tipcalculator.db";
+    public static final int DB_VERSION = 1;
+
+    //list table constants
+    public static final String TIP_TABLE = "tip";
+
+    public static final String TIP_ID = "_id";
+    public static final int TIP_ID_COL = 0;
+
+    public static final String TIP_BILL_DATE = "tip_bill_date";
+    public static final int TIP_BILL_DATE_COL = 1;
+
+    public static final String TIP_BILL_AMOUNT = "tip_bill_amount";
+    public static final int TIP_BILL_AMOUNT_COL = 2;
+
+    public static final String TIP_TIP_PERCENT = "tip_tip_percent";
+    public static final int TIP_TIP_PERCENT_COL = 3;
+
+    public TipCalculatorDB(){
+
+    }
+    //CREATE and DROP TABLE statements
+    public static final String CREATE_TIP_TABLE =
+            "CREATE TABLE " + TIP_TABLE + " (" +
+                    TIP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    TIP_BILL_DATE + " INTEGER NOT NULL, " +
+                    TIP_BILL_AMOUNT + " REAL, " +
+                    TIP_TIP_PERCENT + " REAL);";
+
+    public static final String DROP_TIP_TABLE =
+            "DROP TABLE IF EXISTS " + TIP_TABLE;
+
+
+    private static class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context, String name, CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            //create tables
+            db.execSQL(CREATE_TIP_TABLE);
+            //insert test data
+            db.execSQL("INSERT INTO tip VALUES (1, 0, 40.00, .20)");
+            db.execSQL("INSERT INTO tip VALUES (2, 0, 20.00, .15)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.d("tipcalculator", "Upgrading db from version " + oldVersion + "to " + newVersion);
+
+            db.execSQL(TipCalculatorDB.DROP_TIP_TABLE);
+            onCreate(db);
+        }
+    }//End of DBHelper
+
+    //adding in constructor pg415
+    public TipCalculatorDB(Context context){
+        dbHelper = new DBHelper(context, DB_NAME, null, DB_VERSION);
+    }
+
+    //private methods
+    private void openReadableDB(){
+        db = dbHelper.getReadableDatabase();
+    }
+
+    private void openWriteableDB(){
+        db = dbHelper.getWritableDatabase();
+    }
+
+    private void closeDB(){
+        if(db != null)
+            db.close();
+    }
+
+    public ArrayList<Tip> getTips() {
+
+        this.openReadableDB();
+
+        Cursor cursor = db.query(TIP_TABLE, null, null, null, null, null, null);
+        ArrayList<Tip> tips = new ArrayList<Tip>();
+        while (cursor.moveToNext()){
+            Tip tip = new Tip(
+                    cursor.getLong(TIP_ID_COL),
+                    cursor.getLong(TIP_BILL_DATE_COL),
+                    cursor.getFloat(TIP_BILL_AMOUNT_COL),
+                    cursor.getFloat(TIP_TIP_PERCENT_COL));
+            tips.add(tip);
+        }
+        if(cursor != null){
+            cursor.close();
+        }
+        this.closeDB();
+        return tips;
+    }//end of ArrayList
+
+    public long insertTip(Tip tip){
+        ContentValues cv = new ContentValues();
+        cv.put(TIP_BILL_DATE, System.currentTimeMillis());
+        cv.put(TIP_BILL_AMOUNT, tip.getBillAmount());
+        cv.put(TIP_TIP_PERCENT, tip.getTipPercent());
+
+        this.openWriteableDB();
+        long rowID = db.insert(TIP_TABLE, null, cv);
+        this.closeDB();
+
+        return rowID;
+    }
+
+    /*
+    public static Tip getTipFromCursor(Cursor cursor) {
+        if (cursor == null || cursor.getCount() == 0) {
+            return null;
+        }
+        else{
+            try{
+                Tip tip = new Tip(
+                        cursor.getInt(TIP_ID_COL),
+                        cursor.getInt(TIP_BILL_DATE_COL),
+                        cursor.getInt(TIP_BILL_AMOUNT_COL),
+                        cursor.getInt(TIP_TIP_PERCENT_COL));
+                    return tip;
+                }//end of try
+            catch (Exception e){
+                return null;
+            }//end else
+        }//end of getTipFromCursor
+    */
+
+}//end of class
